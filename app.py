d4890d2b4a2be9f51ea7875ace95ddf20c824fcb
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2023-01-25 10:34:22 -0800

    Base stuff

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import requests
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = "mysql.2223.lakeside-cs.org"
+app.config['MYSQL_USER'] = "student2223"
+app.config['MYSQL_PASSWORD'] = "m545CS42223"
+app.config['MYSQL_DB'] = "2223project_1"
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "secret"
+mysql = MySQL(app)
+
+def executeQuery(query, queryVars):
+    #Initialize database connection
+    cursor = mysql.connection.cursor()
+    #Execute query
+    cursor.execute(query, queryVars)
+    mysql.connection.commit()
+    #Fetch data from query
+    return cursor.fetchall()
