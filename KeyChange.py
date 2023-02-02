d4b6f4e77bd3fa85ffc442e8623ef08100a2e629
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2023-02-02 06:34:58 -0800

    finished up key change function

--- KeyChange.py
+++ KeyChange.py
@@ -1,6 +1,10 @@
 import librosa
 import soundfile as sf
 
-y, sr = librosa.load('hello.wav')
-y_shifted = librosa.effects.pitch_shift(y, sr, n_steps=-4)
-sf.write('keychanged.wav', y_shifted, sr, 'PCM_24')
+def keyChange(filename, numSteps, outputFile):
+    print("***STARTING KEY CHANGE***")
+    y, sr = librosa.load(filename)
+    y_shifted = librosa.effects.pitch_shift(y, sr, n_steps=numSteps)
+    sf.write('static/audio/'+outputFile, y_shifted, sr, 'PCM_24')
+
+keyChange('static/audio/nevergonnabase.wav', 4, 'new.wav')
