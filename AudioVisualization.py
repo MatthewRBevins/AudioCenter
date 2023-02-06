e71725d1f4ad84d754531369e8aee3e9e6d2ff4d
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2023-02-05 18:15:34 -0800

    Spleeter is slow

--- AudioVisualization.py
+++ AudioVisualization.py
@@ -1,14 +1,8 @@
 import librosa
 import numpy as np
+import matplotlib.pyplot as plt
 y, sr = librosa.load("static/audio/nevergonnabase.wav")
-D = librosa.stft(y)
-s = np.abs(librosa.stft(y)**2) # Get magnitude of stft
-chroma = librosa.feature.chroma_stft(S=s, sr=sr)
-chroma = np.cumsum(chroma)
-import matplotlib.pylab as plt
-x = np.linspace(-chroma, chroma)
-plt.plot(x, np.sin(x))
-plt.xlabel('Angle [rad]')
-plt.ylabel('sin(x)')
-plt.axis('tight')
+x = np.arange(0,len(y),1)
+print(x)
+plt.plot(x, y)
 plt.show()
