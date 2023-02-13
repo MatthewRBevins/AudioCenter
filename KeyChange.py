b972921729ec82ca6e0e06c520cb28c43e27b7a8
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2023-02-13 10:31:20 -0800

    keychange

--- KeyChange.py
+++ KeyChange.py
@@ -6,5 +6,6 @@ def keyChange(filename, numSteps, outputFile):
     y, sr = librosa.load(filename)
     y_shifted = librosa.effects.pitch_shift(y, sr, n_steps=numSteps)
     sf.write('static/audio/'+outputFile, y_shifted, sr, 'PCM_24')
+    print("***DONE***")
 
-keyChange('static/audio/nevergonnabase.wav', 4, 'new.wav')
+keyChange('static/audio/nevergonnabase.wav', 4, 'newNeverGonna.wav')
