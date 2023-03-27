c168167ee63bf78649bdadca0ad7028a75900f3e
bonoboTheShrimp <rahulsundaresan@outlook.com>
2023-03-27 10:16:03 -0700

    added import

--- AudioTools.py
+++ AudioTools.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import subprocess
+from scipy.io import wavfile
 
 def mp3towav(file, newfile):
     subprocess.call(['ffmpeg', '-i', file,newfile])
