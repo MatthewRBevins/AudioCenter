e66ea9beefdda5ea49c6a79abf5032412c09f3c7
michaely24 <michaely24@lakesideschool.org>
2023-06-04 08:57:09 -0700

    Progress Bar JS File

--- static/js/progress.js
+++ static/js/progress.js
@@ -1,35 +1,37 @@
-async function alertModal(time){
-  $(document).ready(function() {
-
-    var progress = $('.progressbar .progress')
-
-    function counterInit( fValue, lValue ) {
-
-      var counter_value = parseInt( $('.counter').text() );
-      counter_value++;
-
-      if( counter_value >= fValue && counter_value <= lValue ) {
-
-        $('.counter').text( counter_value + '%' );
-        progress.css({ 'width': counter_value + '%' });
-
-        setTimeout( function() {
-          counterInit( fValue, lValue );
-        }, time );
-
-
-      }
-
-    }
-
-    counterInit( 0, 100 );
-  });
-  await hideBar(100*time + 1500)
+function counterInit( fValue, lValue, time ) {
+  var progress = $('.progressbar .progress')
+  var counter_value = parseInt( $('.counter').text() );
+  console.log(counter_value)
+  counter_value++;
+  if( counter_value >= fValue && counter_value <= lValue ) {
+    $('.counter').text( counter_value + '%' );
+    progress.css({ 'width': counter_value + '%' });
+    setTimeout( function() {
+      counterInit( fValue, lValue, time );
+    }, time );
   }
+}
+
+async function resetBar() {
+  console.log("Resetting");
+  if(parseInt( $('.counter').text() ) == 99) {
+    $('.counter').text( 0 + '%' );
+  }
+}
+
+async function alertModal(time){
+  console.log("Alerting");
+  if(parseInt( $('.counter').text() ) != 0 ) {
+    $('.counter').text( 0 + '%' );
+  }
+  counterInit( 0, 99, time );
+}
 
 function showBar() {
   document.getElementById("bar").style.display = "";
 }
+
+
 async function hideBar(time) {
   return new Promise((res)=>{
     setTimeout(()=>{
